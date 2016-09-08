4e2e3ce02614f5bd13fe13887de2c0af6d2c679a
Ben Ashworth <bashworth16@yahoo.com>
2016-09-07 21:53:55 -0500

    Working Game

--- numbersgame.py
+++ numbersgame.py
@@ -1,16 +1,36 @@
 #/usr/bin/env python3
+
 import random
-number = random.randint(1, 100)
-guess = int()
-while (guess != number):
 
-    guess = input("Guess a number between 1 and 100: ")
+def get_random():
+    return random.randint(1, 100) 
 
-    if int(guess) > int(number):
-        print("Lower...: ")
-    if int(guess) < int(number):
-        print("Higher...: ")
-    if int(guess) == int(number):
-        input("You're Right!")
-        break
+def get_guess():
+    try:
+        return int(input("Guess: "))
+    except ValueError:
+        print("Numbers only Please! Try again!")
+        return get_guess()
 
+def main():
+    number = random.randint(1, 100)
+    guess = get_guess()
+    while guess != number:
+        if guess > number:
+            print('Lower')
+        elif guess < number:
+            print('Higher')
+        guess = get_guess()
+    print('Correct!')
+    pa()
+
+def pa():
+    play_again = input("Would you like to play again? y/n: ")
+    if play_again == "y":
+        return main()
+    else:
+        print('Good Bye!')
+        
+
+if __name__ == '__main__':
+    main()
